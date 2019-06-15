2f462dc0539de3102d9ddf909388e37888bf99d1
pinheirodps <pinheirodps@gmail.com>
2019-06-15 21:15:37 +0100

    stop aula 47

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -4,9 +4,13 @@ import {Http} from '@angular/http'
 import {MEAT_API} from '../../app.api';
 import {Observable} from 'rxjs';
 import 'rxjs/add/operator/map'
+import "rxjs/add/operator/catch"
+import {ErrorHandler} from "../app.error-handler"
 
 @Injectable()
 export class RestaurantsService {
+
+
 
   rests: Restaurant[]= [
 
@@ -34,6 +38,12 @@ export class RestaurantsService {
   restaurants(): Observable<Restaurant[]> {
     return this.http.get(`${MEAT_API}/restaurants`)
       .map(response => response.json())
+      .catch(ErrorHandler.handleError)
   }
 
+  restaurantById(id: string): Observable<Restaurant>{
+    return this.http.get(`${MEAT_API}/restaurants/${id}`)
+      .map(response => response.json())
+      .catch(ErrorHandler.handleError)
+  }
 }
