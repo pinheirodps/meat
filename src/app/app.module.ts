2e787b55cb466b78efdf949d6ece9ff7225a7817
pinheirodps <pinheirodps@gmail.com>
2019-06-20 18:07:30 +0100

    stop aula 92

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import {LOCALE_ID, NgModule} from '@angular/core';
-import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+import {BrowserModule} from '@angular/platform-browser'
+import {LOCALE_ID, NgModule} from '@angular/core'
+import {HttpModule} from '@angular/http'
+import {PreloadAllModules, RouterModule} from '@angular/router'
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations"
 
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component'
-import {ROUTES} from './app.routes';
-import { RestaurantsComponent } from './restaurants/restaurants.component';
-import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
-import {RestaurantsService} from './restaurants/restaurants.service';
-import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
-import { MenuComponent } from './restaurant-detail/menu/menu.component';
-import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
-import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
-import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
-import {ShoppingCartService} from './restaurant-detail/shopping-cart/shopping-cart.service';
-import { OrderComponent } from './order/order.component';
-import {FormsModule} from '@angular/forms';
-import { InputComponent } from './shared/input/input.component';
-import { RadioComponent } from './shared/radio/radio.component';
-import { OrderItemsComponent } from './order/order-items/order-items.component'
-import {OrderService} from './order/order.service';
-import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
+import {AppComponent} from './app.component'
+import {HeaderComponent} from './header/header.component'
+import {HomeComponent} from './home/home.component'
+import {ROUTES} from './app.routes'
+import {RestaurantsComponent} from './restaurants/restaurants.component'
+import {RestaurantComponent} from './restaurants/restaurant/restaurant.component'
+import {RestaurantDetailComponent} from './restaurant-detail/restaurant-detail.component'
+import {MenuComponent} from './restaurant-detail/menu/menu.component'
+import {ShoppingCartComponent} from './restaurant-detail/shopping-cart/shopping-cart.component'
+import {MenuItemComponent} from './restaurant-detail/menu-item/menu-item.component'
+import {ReviewsComponent} from './restaurant-detail/reviews/reviews.component'
+import {OrderSummaryComponent} from "./order/order-summary/order-summary.component"
+import {SharedModule} from "./shared/shared.module"
 
 
 @NgModule({
@@ -32,7 +25,6 @@ import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.co
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    AboutComponent,
     RestaurantsComponent,
     RestaurantComponent,
     RestaurantDetailComponent,
@@ -40,19 +32,16 @@ import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.co
     ShoppingCartComponent,
     MenuItemComponent,
     ReviewsComponent,
-    OrderComponent,
-    InputComponent,
-    RadioComponent,
-    OrderItemsComponent,
-    DeliveryCostsComponent
+    OrderSummaryComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    FormsModule,
-    RouterModule.forRoot(ROUTES)
+    BrowserAnimationsModule,
+    SharedModule.forRoot(),
+    RouterModule.forRoot(ROUTES, {preloadingStrategy: PreloadAllModules})
   ],
-  providers: [RestaurantsService, ShoppingCartService, {provide: LOCALE_ID, useValue: 'pt-BR'}, OrderService],
+  providers: [ {provide: LOCALE_ID, useValue: 'pt-BR'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,10 @@
 import {Routes} from '@angular/router';
 import {HomeComponent} from './home/home.component';
-import {AboutComponent} from './about/about.component';
 import {RestaurantsComponent} from './restaurants/restaurants.component';
 import {RestaurantDetailComponent} from "./restaurant-detail/restaurant-detail.component"
 import {MenuComponent} from "./restaurant-detail/menu/menu.component"
 import {ReviewsComponent} from "./restaurant-detail/reviews/reviews.component"
-import {OrderComponent} from './order/order.component';
+import {OrderSummaryComponent} from "./order/order-summary/order-summary.component"
 
 export const ROUTES: Routes = [
   {path: '', component: HomeComponent},
@@ -16,6 +15,7 @@ export const ROUTES: Routes = [
       {path: 'menu', component: MenuComponent},
       {path: 'reviews', component: ReviewsComponent}
     ]},
-  {path: 'order', component: OrderComponent},
-  {path: 'about', component: AboutComponent},
+  {path: 'order', loadChildren: './order/order.module#OrderModule'},
+  {path: 'order-summary', component: OrderSummaryComponent},
+  {path: 'about', loadChildren: './about/about.module#AboutModule'},
 ];
